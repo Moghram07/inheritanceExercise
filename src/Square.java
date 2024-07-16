338481d7397549473a4e73b000a58cee7e93cff8
OMAR ALSHEHRI <166976917+Moghram07@users.noreply.github.com>
2024-07-16 11:50:09 +0300

    Update Square.java
    square class updated

--- src/Square.java
+++ src/Square.java
@@ -1,31 +1,31 @@
 public class Square extends Rectangle{
-    private double side;
+   // private double side;
     Square(){
     }
     Square(double side){
-        this.side = side;
+        super.setWidth(side);
     }
     Square(double side, String color, boolean filled){
         super(side, side,color, filled);
-        this.side = side;
+
     }
 
     public double getSide() {
-        return side;
+        return super.getWidth();
     }
 
     public void setSide(double side) {
-        this.side = side;
+        super.setWidth(side);
     }
 
     public void getArea(){
-        double area = side * side;
+        double area = super.getWidth() * super.getWidth();
         System.out.println("Square area : "+ area);
     }
 
 
     @Override
     public String toString() {
-        return "Square[side=" + side + ", " + super.toString() + "]";
+        return "Square[side=" + super.getWidth() + ", " + super.toString() + "]";
     }
 }
